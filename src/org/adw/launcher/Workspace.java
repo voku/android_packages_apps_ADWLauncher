a3e89e24196166ff988b74ce8a7bc7b09432c5e3
adw <adw@ubuntu-desktop.(none)>
2010-05-31 13:21:31 +0200

    workspace should draw at least wallpaper if showing previews/app drawer

--- src/org/adw/launcher/Workspace.java
+++ src/org/adw/launcher/Workspace.java
@@ -447,12 +447,6 @@ public class Workspace extends ViewGroup implements DropTarget, DragSource, Drag
     @Override
     protected void dispatchDraw(Canvas canvas) {
         boolean restore = false;
-        // If the all apps drawer is open and the drawing region for the workspace
-        // is contained within the drawer's bounds, we skip the drawing. This requires
-        // the drawer to be fully opaque.
-        if((mLauncher.isAllAppsVisible() && mLauncher.isAllAppsOpaque()) || mLauncher.isFullScreenPreviewing()){
-        	return;
-        }
         //ADW: If using old wallpaper rendering method...
         if(!lwpSupport && mWallpaper!=null){
         	float x = getScrollX() * mWallpaperOffset;
@@ -465,6 +459,12 @@ public class Workspace extends ViewGroup implements DropTarget, DragSource, Drag
         		x=(getScrollX()-mWallpaperWidth+(getRight()-getLeft()));
         	}
     		canvas.drawBitmap(mWallpaper, x, (getBottom() - mWallpaperHeight) / 2, mPaint);
+        }
+        // If the all apps drawer is open and the drawing region for the workspace
+        // is contained within the drawer's bounds, we skip the drawing. This requires
+        // the drawer to be fully opaque.
+        if((mLauncher.isAllAppsVisible() && mLauncher.isAllAppsOpaque()) || mLauncher.isFullScreenPreviewing()){
+        	return;
         }
         // ViewGroup.dispatchDraw() supports many features we don't need:
         // clip to padding, layout animation, animation listener, disappearing
