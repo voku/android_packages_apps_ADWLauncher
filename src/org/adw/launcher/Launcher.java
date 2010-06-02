cfd71cde08913a557bd0ed1a39e09fded12ef26f
adw <adw@ubuntu-desktop.(none)>
2010-06-02 09:38:01 +0200

    Fixed the showingPreviews flag to set only if previews are really shown... That should fix the IllegalArgumentException some users are getting when hitting Home button before launcher is fully loaded

--- src/org/adw/launcher/Launcher.java
+++ src/org/adw/launcher/Launcher.java
@@ -2800,7 +2800,6 @@ public final class Launcher extends Activity implements View.OnClickListener, On
         //check first if it's already open
         final PopupWindow window = (PopupWindow) anchor.getTag();
         if (window != null) return;
-        showingPreviews=true;
     	Resources resources = getResources();
 
         Workspace workspace = mWorkspace;
@@ -2833,6 +2832,7 @@ public final class Launcher extends Activity implements View.OnClickListener, On
         //width -= (x + cell.getRightPadding());
         //height -= (y + cell.getBottomPadding());
         if(width!=0 && height!=0){
+            showingPreviews=true;
 	        float scale = w / width;
 	
 	        int count = end - start;
