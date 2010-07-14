fd72b1e87b00955b928629c5ac48ab7738901da9
AnderWeb <anderweb@gmail.com>
2010-07-14 18:50:48 +0200

    Update scrollable code from launcher+ to r22

--- src/org/adw/launcher/LauncherAppWidgetHostView.java
+++ src/org/adw/launcher/LauncherAppWidgetHostView.java
@@ -28,7 +28,8 @@ import android.view.ViewConfiguration;
  * {@inheritDoc}
  */
 public class LauncherAppWidgetHostView extends AppWidgetHostView {
-    private boolean mHasPerformedLongPress;
+	private static final long WIDGET_LONG_CLICK_TIMEOUT = 2000;
+	private boolean mHasPerformedLongPress;
     
     private CheckForLongPress mPendingCheckForLongPress;
     
@@ -97,7 +98,8 @@ public class LauncherAppWidgetHostView extends AppWidgetHostView {
             mPendingCheckForLongPress = new CheckForLongPress();
         }
         mPendingCheckForLongPress.rememberWindowAttachCount();
-        postDelayed(mPendingCheckForLongPress, ViewConfiguration.getLongPressTimeout());
+        //postDelayed(mPendingCheckForLongPress, ViewConfiguration.getLongPressTimeout());
+        postDelayed(mPendingCheckForLongPress, WIDGET_LONG_CLICK_TIMEOUT);
     }
 
     @Override
