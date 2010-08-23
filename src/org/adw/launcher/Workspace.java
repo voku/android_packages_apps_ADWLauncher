1fbc86b070d5e25e9b35b4f0da5ea4075940f04f
AnderWeb <anderweb@gmail.com>
2010-08-23 22:51:10 +0200

    Few fixes:
    -Avoid possible NPE when the launcher is not yet visible trying to send a LWP wallpapercommand
    -Fix the default screen check when removing screens, it caused NPEs when the default screen was set to the last one.
    -Fix a ClassCastException in the app catalogs code.
    -Small spanish typo.

--- src/org/adw/launcher/Workspace.java
+++ src/org/adw/launcher/Workspace.java
@@ -1895,7 +1895,7 @@ public class Workspace extends WidgetSpace implements DropTarget, DragSource, Dr
 	 * @param hide
 	 */
     public void hideWallpaper(boolean hide) {
-    	if(getWindowToken()!=null){
+    	if(getWindowToken()!=null && mLauncher.getWindow()!=null){
 	    	if (hide){
 	    		mWallpaperManager.sendWallpaperCommand(getWindowToken(),
 	    				"hide", 0, 0, 0, null);
@@ -1945,7 +1945,7 @@ public class Workspace extends WidgetSpace implements DropTarget, DragSource, Dr
         	mCurrentScreen=0;
         	setCurrentScreen(mCurrentScreen);
         }
-        if(getChildCount()<mDefaultScreen){
+        if(getChildCount()<=mDefaultScreen){
         	AlmostNexusSettingsHelper.setDefaultScreen(mLauncher, 0);
         	mDefaultScreen=0;
         }
