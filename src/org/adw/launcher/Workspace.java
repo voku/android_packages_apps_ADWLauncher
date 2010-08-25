387ffdcdcc6913d205f23bd6e2b14c4b3f86e6c7
AnderWeb <anderweb@gmail.com>
2010-08-25 13:46:32 +0200

    Fix NPE's cause of adding/removing/swapping screens
    CellLayout stored the "mCellInfo.screen" field when "onAttachedToWindow", and use it when longpressing a screen to add items.
    But changing screens didn't change that internal field so added a getter/setter to change the value when needed.

--- src/org/adw/launcher/Workspace.java
+++ src/org/adw/launcher/Workspace.java
@@ -1967,6 +1967,7 @@ public class Workspace extends WidgetSpace implements DropTarget, DragSource, Dr
     protected void swapScreens(int screen_a, int screen_b){
     	//Swap database positions for both screens
         CellLayout layout = (CellLayout) getChildAt(screen_a);
+        layout.setScreen(screen_b);
         int childCount = layout.getChildCount();
         for (int j = 0; j < childCount; j++) {
             final View view = layout.getChildAt(j);
@@ -1977,6 +1978,7 @@ public class Workspace extends WidgetSpace implements DropTarget, DragSource, Dr
             }
         }
         layout = (CellLayout) getChildAt(screen_b);
+        layout.setScreen(screen_a);
         childCount = layout.getChildCount();
         for (int j = 0; j < childCount; j++) {
             final View view = layout.getChildAt(j);
@@ -1997,6 +1999,7 @@ public class Workspace extends WidgetSpace implements DropTarget, DragSource, Dr
         //MOVE THE REMAINING ITEMS FROM OTHER SCREENS
         for (int i=screen+1;i<getChildCount();i++){
             final CellLayout layout = (CellLayout) getChildAt(i);
+            layout.setScreen(layout.getScreen()+diff);
             int childCount = layout.getChildCount();
             for (int j = 0; j < childCount; j++) {
                 final View view = layout.getChildAt(j);
